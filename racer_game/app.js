f2311d2ef0a9916ce0201478c249c385b7ae0be0
Tess Craig <tessashleycraig89@gmail.com>
2017-12-21 01:10:15 -0600

    Commit 2 - refractors js and adds jquery

--- racer_game/app.js
+++ racer_game/app.js
@@ -1,50 +1,58 @@
+function GetPlayers() {
+  this.player1   = $("#player1");
+  this.player2   = $("#player2");
+}
+let myGetPlayers = new GetPlayers();
 
-//define player1
-const player1 = document.getElementById('player1');
-//define player2
-const player2 = document.getElementById('player2');
-//define player1 initial position
-let player1Pos = 0;
-//define player2 initial position
-let player2Pos = 0;
-//define finishLine position in relation to screen width
-const finishLinePos = window.screen.width - 150;
-//get finishLine element and style finishLinePos to the right side of the screen at finishLinePos px
-document.getElementById("finishLine").setAttribute("style", "left: " + finishLinePos + "px");
 
-//announce function
-    //if player1 postion >= finishLinePos
-        //alert player1 won
-    //else if player2 postion >= finishLinePos
-      //alert player2 won
-const announceWinner = () => {
-    if (player1Pos >= finishLinePos) {
-      alert("Player 1 is the winner!");
-    } else if (player2Pos >= finishLinePos) {
-      alert("Player 2 is the winner!");
-    }
+function PlayerPos() {
+  this.player1Pos = 0;
+  this.player2Pos = 0;
+}
+let myPlayerPos   = new PlayerPos();
+
+
+function FinishLine() {
+  this.finishLinePos = window.screen.width - 150;
+  this.setAttribute  = function() {
+    $("#finishLine").setAttribute("style", "left: " + this.finishLinePos + "px");
   }
+}
 
-//move function
-  //keypress EventListener
-        // if key === 'z'
-          //player1 position = current postition+ 15
-          //player1.setAttribute for moving right
-        // if key === 'p'
-          //player2 position = current position+ 15
-          //player2.setAttribute for moving right
-const movePlayers = () => {
-  document.addEventListener('keypress', (event) => {
+let myFinishLine     = new FinishLine();
+
+
+
+
+function AnnounceWinner() {
+    this.message = function() {
+      if (this.player1Pos >= this.finishLinePos) {
+      return "Player 1 is the winner!";
+    } else if (this.player2Pos >= this.finishLinePos) {
+      return "Player 2 is the winner!";
+    }
+  };
+}
+
+
+
+let myAnnounceWinner = new AnnounceWinner();
+
+function MovePlayers() {
+  this.addEvent = function() {
+    document.keypress(function() {
       if (event.key === 'z') {
-        player1Pos = player1Pos + 15;
-        player1.setAttribute("style", "position: relative; left: " + player1Pos + "px");
-        announceWinner();
+        this.player1Pos = this.player1Pos + 15;
+        this.player1.setAttribute("style", "position: relative; left: " + this.player1Pos + "px");
+        AnnounceWinner();
       } else if (event.key === 'p') {
-        player2Pos = player2Pos + 15;
-        player2.setAttribute("style", "position: relative; left: " + player2Pos + "px");
-        announceWinner();
+        this.player2Pos = this.player2Pos + 15;
+        this.player2.setAttribute("style", "position: relative; left: " + this.player2Pos + "px");
+        AnnounceWinner();
       }
     });
   }
-// evoke movePlayers function
-movePlayers();
+}
+
+let myMovePlayers = new MovePlayers();
+MovePlayers();
